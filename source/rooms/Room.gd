96a5c24b6bfce071dd677c11e01779247625a374
Rafael Gauna Trindade <rtrindade@esss.co>
2023-02-04 16:40:44 -0300

    Add dynamic room exchanging

--- source/rooms/Room.gd
+++ source/rooms/Room.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
+var current_dir = ""
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	current_dir = get_tree().current_scene.filename.replace("room.tscn", "")
+	
 	for child in get_children():
 		if child.name.substr(0, 4) == "Door":
 			child.connect("on_Door_open", self, "_on_Door_open")
@@ -11,5 +15,6 @@ func _ready():
 #func _process(delta):
 #	pass
 
-func _on_Door_open(dir_name):
-	print(dir_name)
+func _on_Door_open(new_dir_name):
+	var new_scene = current_dir + new_dir_name + "/room.tscn"
+	get_tree().change_scene(new_scene)
