c991bce005051e600a0f91da49fa7d88e9de9d86
Rafael Gauna Trindade <rtrindade@esss.co>
2023-02-05 11:18:18 -0300

    Add a little parsing on terminal code

--- source/main/Terminal.gd
+++ source/main/Terminal.gd
@@ -6,24 +6,41 @@ extends Control
 # var b = "text"
 onready var history: RichTextLabel = $ColorRect/RichTextLabel
 onready var _input: LineEdit = $ColorRect/LineEdit
-var prompt = "\nuser@archie:~$ "
+var prompt = "\n[color=green]user@archie:~$[/color] "
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	history.text = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
-	history.text += prompt
+	history.bbcode_text = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+	history.bbcode_text += prompt
 
-func _on_text_entered(new_text):
+func _on_text_entered(new_text: String):
 	_input.clear()
-
-	if new_text == "":
+	
+	var command = new_text.strip_edges()
+	if command.empty():
 		return
 
-	if _validate(new_text):
-		history.bbcode_text += prompt + new_text
+	history.bbcode_text += prompt + command
+	if _validate(command):
+		history.bbcode_text += run_command(command)
 	else:
-		history.bbcode_text += "\n[color=red]" + new_text + "[/color] : invalid command"
+		history.bbcode_text += "\n[color=red]" + command + "[/color] : invalid command"
 
 func _validate(command: String) -> bool:
-	return command == "cat"
+	var exec = command.substr(0, command.find(" "))
+	return exec in ["cat"]
+
+func run_command(command: String) -> String:
+	var exec_end = command.find(" ")
+	var exec = command.substr(0, exec_end)
+	var args = [] if (exec_end == -1) else command.substr(exec_end + 1).split(" ")
+
+	if exec == "cat":
+		return cat(args)
+	else:
+		return "Gurizada esqueceu de cobrir esse comando"
 		
+func cat(args: Array) -> String:
+	if args.empty():
+		return "\n[color=red]Usage:[/color] cat <file>"
+	return "\n"
